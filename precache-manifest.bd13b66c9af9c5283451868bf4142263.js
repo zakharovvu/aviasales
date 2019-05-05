e9c4c35fc7da759d65ac465379e04766d0262f17
Slava <zakharovvu@gmail.com>
2019-05-05 14:49:00 +0300

    Updates

---
+++ precache-manifest.bd13b66c9af9c5283451868bf4142263.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b006327d6ca16a15e5c145c1f44c699",
+    "url": "/aviasales/index.html"
+  },
+  {
+    "revision": "9b2b3911443753cd9b9c",
+    "url": "/aviasales/static/css/main.311eec8b.chunk.css"
+  },
+  {
+    "revision": "752488559bd1fdd2eb01",
+    "url": "/aviasales/static/js/2.1617241c.chunk.js"
+  },
+  {
+    "revision": "9b2b3911443753cd9b9c",
+    "url": "/aviasales/static/js/main.8b1b38bf.chunk.js"
+  },
+  {
+    "revision": "af052bb48cec26b9b243",
+    "url": "/aviasales/static/js/runtime~main.f8f776b2.js"
+  },
+  {
+    "revision": "ab82d1e50e14a2741ad9156ae57124cd",
+    "url": "/aviasales/static/media/logo.ab82d1e5.png"
+  }
+]);
